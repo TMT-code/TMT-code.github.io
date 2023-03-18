6d775ee055bbbc81e73e6a609cac1877130295b8
TMT-code <84065310+TMT-code@users.noreply.github.com>
2023-03-18 20:12:23 +0700

    chinh sua tum lum

--- js/main.js
+++ js/main.js
@@ -26,3 +26,66 @@ function dropDown() {
       }
     }
   }
+ // dang ky
+  function signup(e){
+    event.preventDefault();
+    var password= document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    var block = document.getElementById("notify");
+    var cpass = document.getElementById("cpassword").value;
+    
+
+    var user = {
+      email: email,
+      password: password,
+      
+    };
+    var json = JSON.stringify(user);
+    if(cpass !== password){
+      block.innerHTML="Confirm password is incorrect !";
+      return false;
+    }
+
+    else {
+    localStorage.setItem(email, json);
+    alert("Registered is sucessfully! Please login to continue. ");
+    window.location.href="/html/form/login.html";
+
+  }
+}
+// so sanh password
+  function compare_password(pass, cpass){
+    if(pass !== cpass){
+      return false;
+    }
+    return true;
+  }
+
+  function display(){
+    var cpass = document.getElementById("cpassword").value;
+    var pass= document.getElementById("password").value;
+    var block = document.getElementById("notify");
+
+    if(compare_password(pass, cpass) == false){
+      document.getElementById("notify").innerHTML="Confirm password is incorrect !";
+    }
+    else block.innerHTML="";
+  }
+// dang nhap
+  function login(e){
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    var result = document.getElementById("notify");
+
+    var user = localStorage.getItem(email);
+    var data = JSON.parse(user);
+
+    if(user != null && pass == data.password){
+      window.location.href = "../navbar2/home.html";
+    }
+    else {
+      result.innerHTML = "Email or password are incorrect!";
+    }
+
+  }
